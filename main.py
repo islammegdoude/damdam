9503edcc1507ea837f43dd4c4acbfacb3104b1ae
islam megdoude <salaheddine@univ-msila.dz>
2021-12-16 00:32:04 +0100

    hem hem

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-def myfunc():
-    global x
-    x = "fantastic"
-
-
-myfunc()
-
-print("Python is " + x)
+thisdict = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+}
+for x in thisdict.items():
+    print(x)
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name": "Linus",
+  "year": 2011
+}
+myfamily = {
+    "child1": child1,
+    "child2": child2,
+    "child3": child3,
+}
+print(myfamily)
